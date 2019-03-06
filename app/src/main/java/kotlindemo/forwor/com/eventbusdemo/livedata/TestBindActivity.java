20af41bd98c58c03d3a0296b53ad976e6fb72082
myy <2635131590@qq.com>
2019-03-06 08:58:59 +0800

    livedata摸索实现

--- app/src/main/java/kotlindemo/forwor/com/eventbusdemo/livedata/TestBindActivity.java
+++ app/src/main/java/kotlindemo/forwor/com/eventbusdemo/livedata/TestBindActivity.java
@@ -1,7 +1,9 @@
 package kotlindemo.forwor.com.eventbusdemo.livedata;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,17 +13,24 @@ import kotlindemo.forwor.com.eventbusdemo.databinding.ActivityTestBindBinding;
 public class TestBindActivity extends AppCompatActivity {
     ActivityTestBindBinding mBinding;
     TestViewModel mTestViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_test_bind);
-         mTestViewModel = ViewModelProviders.of(this).get(TestViewModel.class);
-         //User user = new User();
-         //mTestViewModel.setUser(user);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_test_bind);
+        mTestViewModel = ViewModelProviders.of(this).get(TestViewModel.class);
+        //User user = new User();
+        //mTestViewModel.setUser(user);
         //需要持有同一种数据对象(binding和viewmodel)
-         mTestViewModel.setBinding(mBinding);
-         mBinding.setUser(mTestViewModel.getUser().getValue());
-         mBinding.setViewModel(mTestViewModel);
-         mBinding.setLifecycleOwner(this);
+        Observer<User> user = new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                mBinding.setUser(user);
+            }
+        };
+        mTestViewModel.getUser().observe(this, user);
+        mBinding.setUser(mTestViewModel.getUser().getValue());//为databing设置数据源
+        mBinding.setViewModel(mTestViewModel);
+        mBinding.setLifecycleOwner(this);
     }
 }
--- app/src/main/java/kotlindemo/forwor/com/eventbusdemo/livedata/TestViewModel.java
+++ app/src/main/java/kotlindemo/forwor/com/eventbusdemo/livedata/TestViewModel.java
@@ -3,47 +3,38 @@ package kotlindemo.forwor.com.eventbusdemo.livedata;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.MutableLiveData;
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Toast;
-
-import kotlindemo.forwor.com.eventbusdemo.TestOneActivity;
-import kotlindemo.forwor.com.eventbusdemo.databinding.ActivityTestBindBinding;
 
 /**
  * Created by Myy on 2019/3/5 10:09
  */
 public class TestViewModel extends AndroidViewModel {
-    private ActivityTestBindBinding binding;
-    private MutableLiveData<User> mUser = new MutableLiveData<>();
+    private MutableLiveData<User> mUser;
 
     public TestViewModel(@NonNull Application application) {
         super(application);
     }
 
-    public void setBinding(ActivityTestBindBinding binding) {
-        this.binding = binding;
-    }
-
     MutableLiveData<User> getUser() {//网络层获取数据
+        if (mUser == null) mUser = new MutableLiveData<>();
+        //网络请求，然后给mUser赋值
         User user = new User();
-        user.setUserName("李四");
-        setUser(user);
-        return mUser;
-    }
-
-    private void setUser(User user) {//这个可以不要
+        user.setUserName("小明");
         mUser.setValue(user);
+        return mUser;
     }
 
     public void onBtnClick(View view) {
         if (mUser.getValue() != null) {
-                User user = mUser.getValue();
-                user.setUserName("小风");
-                binding.setUser(user);
-                Toast.makeText(getApplication(), mUser.getValue().getUserName(), Toast.LENGTH_SHORT).show();
-                view.getContext().startActivity(new Intent(view.getContext(),TestOneActivity.class));
+            //数据变化改变UI
+          /*  User user = getUser().getValue();
+            if (user != null) user.setUserName("小风");
+            mUser.setValue(user);*/
+           //ui上数据变化后保存到数据源
+            Toast.makeText(getApplication(), mUser.getValue().getUserName(), Toast.LENGTH_SHORT).show();
+            //view.getContext().startActivity(new Intent(view.getContext(),TestOneActivity.class));
         } else Toast.makeText(getApplication(), "哈哈", Toast.LENGTH_SHORT).show();
     }
 
